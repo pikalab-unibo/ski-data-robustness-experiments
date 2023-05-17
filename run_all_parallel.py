929243f89311ae040480046d0325a8b02255e944
AndAgio <aagiollo@gmail.com>
2023-05-17 18:18:39 +0200

    fix: create logs directory.

--- run_all_parallel.py
+++ run_all_parallel.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 datas = ["b", "c", "s"]
@@ -5,9 +6,10 @@ degradations = ["d", "n", "l", "m"]
 predictors = ["u", "kins", "kill", "kbann"]
 commands = ["python setup.py run_experiments -d {} -t {} -p {}".format(d, e, p) for d in datas for e in degradations for
             p in predictors]
-print('commands are {}'.format(commands))
 logfiles = ["logs/d={}-e={}-p={}.txt".format(d, e, p) for d in datas for e in degradations for p in predictors]
-print('logfiles are {}'.format(logfiles))
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
 
 procs = [subprocess.Popen(commands[i], shell=True, stdout=open(logfiles[i], "w")) for i in range(len(commands))]
 for p in procs:
