6793d81314986c199e11575ec6131d3740353171
AndAgio <aagiollo@gmail.com>
2023-05-25 16:53:54 +0200

    feat: compute all divergences and all robustness scores in parallel.

--- run_all_parallel.py
+++ run_all_parallel.py
@@ -1,16 +1,43 @@
 import os
 import subprocess
+import argparse
 
-datas = ["b", "c", "s"]
-degradations = ["d", "n", "l", "m"]
-predictors = ["u", "kins", "kill", "kbann"]
-commands = ["python setup.py run_experiments -d {} -t {} -p {}".format(d, e, p) for d in datas for e in degradations for
-            p in predictors]
-logfiles = ["logs/d={}-e={}-p={}.txt".format(d, e, p) for d in datas for e in degradations for p in predictors]
 
-if not os.path.exists('logs'):
-    os.makedirs('logs')
+def run_all_in_parallel(coms, files):
+    if not os.path.exists('logs'):
+        os.makedirs('logs')
+    procs = [subprocess.Popen(coms[i], shell=True, stdout=open(files[i], "w")) for i in range(len(commands))]
+    for p in procs:
+        p.wait()
 
-procs = [subprocess.Popen(commands[i], shell=True, stdout=open(logfiles[i], "w")) for i in range(len(commands))]
-for p in procs:
-    p.wait()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Run all in parallel options",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-e", "--experiment", type=str, default='train', help="train to run all training, "
+                                                                              "kl to run all divergences, "
+                                                                              "metric to compute all metrics")
+    options = parser.parse_args()
+
+    if options.experiment.lower() in ['train', 't']:
+        datas = ["b", "c", "s"]
+        degradations = ["d", "n", "l", "m"]
+        predictors = ["u", "kins", "kill", "kbann"]
+        commands = ["python setup.py run_experiments -d {} -t {} -p {}".format(d, e, p) for d in datas
+                    for e in degradations
+                    for p in predictors]
+        logfiles = ["logs/d={}-e={}-p={}.txt".format(d, e, p) for d in datas for e in degradations for p in predictors]
+        run_all_in_parallel(commands, logfiles)
+    elif options.experiment.lower() in ['kl', 'divergence']:
+        datas = ["b", "c", "s"]
+        degradations = ["d", "n", "l", "m"]
+        commands = ["python setup.py run_divergence -d {} -t {}".format(d, e) for d in datas for e in degradations]
+        logfiles = ["logs/kl of d={}-e={}.txt".format(d, e) for d in datas for e in degradations]
+        run_all_in_parallel(commands, logfiles)
+    elif options.experiment.lower() in ['metric', 'robustness']:
+        degradations = ["d", "n", "l", "m"]
+        commands = ["python setup.py compute_robustness -t {}".format(e) for e in degradations]
+        logfiles = ["logs/robustness computation of e={}.txt".format(e) for e in degradations]
+        run_all_in_parallel(commands, logfiles)
+
+
