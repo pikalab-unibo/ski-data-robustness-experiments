5ed245fa541f5ccdf914a51518e9218b6e6d1f1e
AndAgio <aagiollo@gmail.com>
2023-06-03 07:48:42 +0100

    feat: new results label flip

--- run_all_parallel.py
+++ run_all_parallel.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
     if options.experiment.lower() in ['train', 't']:
         datas = ["b", "c", "s"]
-        degradations = ["d", "n", "l", "m"]
+        degradations = ["l"]  # ["d", "n", "l", "m"]
         predictors = ["u", "kins", "kill", "kbann"]
         commands = ["python setup.py run_experiments -d {} -t {} -p {}".format(d, e, p) for d in datas
                     for e in degradations
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         run_all_in_parallel(commands, logfiles)
     elif options.experiment.lower() in ['kl', 'divergence']:
         datas = ["b", "c", "s"]
-        degradations = ["d", "n", "l"]  # , "m"]
+        degradations = ["l"]  # ["d", "n", "l", "m"]
         commands = ["python setup.py run_divergence -d {} -t {}".format(d, e) for d in datas for e in degradations]
         logfiles = ["logs/kl of d={}-e={}.txt".format(d, e) for d in datas for e in degradations]
         run_all_in_parallel(commands, logfiles)
